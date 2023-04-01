b6c074568720ba8c3eb9c8c3aae7cde529e3444b
Chris0Jeky <jeky.tck@gmail.com>
2023-04-01 23:35:17 +0100

    Basic model: User created

--- server.js
+++ server.js
@@ -3,6 +3,39 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://kali:kali@clustertest.bziibod.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+const User = require('./models/User');
+
+app.get('/api/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching users' });
+    }
+});
+
+// Add other routes for creating, updating, and deleting data
+
+
 // Middleware for parsing JSON requests
 app.use(express.json());
 
