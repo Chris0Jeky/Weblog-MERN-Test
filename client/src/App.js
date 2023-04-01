a3008f7180104046a4bee595dba881713b2375c1
Chris0Jeky <jeky.tck@gmail.com>
2023-04-01 23:18:19 +0100

    Connectivity tested successfully

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,43 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ApiTest from './ApiTest';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('/api/message');
+        const data = await response.text();
+        setMessage(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ApiTest />
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <h2>API Test</h2>
+          <p>Message from the server: {message}</p>
+        </header>
+      </div>
   );
 }
 
--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-const path = require('path');
-
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Middleware for parsing JSON requests
+app.use(express.json());
+
+app.get('/api/message', (req, res) => {
+    res.json({ message: 'Hello from the server!' });
 });
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-
-app.get('/api/test', (req, res) => {
-    res.json({ message: 'Hello from the server!' });
-});
-
