08cfffb41d8ad026c4b20b5cc8cd79bf0363b219
Chris0Jeky <jeky.tck@gmail.com>
2023-04-01 22:40:19 +0100

    Wiring up server to client

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,4 +10,10 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
